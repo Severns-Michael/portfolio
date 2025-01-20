571347606c9fedbc4905ce583419a743fd215c4e
Michael Severns <Severns0319@gmail.com>
2025-01-20 13:15:33 -0500

    clean up

---
+++ java/src/main/java/com/portfolio/dao/UserDao.java
@@ -0,0 +1,17 @@
+package com.portfolio.dao;
+
+import com.portfolio.model.RegisterUserDto;
+import com.portfolio.model.User;
+
+import java.util.List;
+
+public interface UserDao {
+
+    List<User> getUsers();
+
+    User getUserById(int id);
+
+    User getUserByUsername(String username);
+
+    User createUser(RegisterUserDto user);
+}
