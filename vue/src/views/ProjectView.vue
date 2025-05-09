ad31ae1e795e9a2783063b8dd3eaa9cc75b4fa46
michael severns <severns0319@gmail.com>
2025-05-09 19:14:05 -0400

    production site vails to load iamges

--- vue/src/views/ProjectView.vue
+++ vue/src/views/ProjectView.vue
@@ -45,9 +45,9 @@ export default {
           title: "Tower Defense Game",
           description: "A custom-built Tower Defense game featuring a React UI and PhaserJS game engine. Players strategically place and upgrade towers across elemental paths—Fire, Ice, Lightning, and Physical—to defend against increasingly challenging waves of enemies (Grunts, Fast, Brutes, Bosses). Each tower has unique modifiers Like Freeze, Burn, Chain Lightning. Includes a visual tile-based level editor, upgrade path system (T3 max path + T2 secondary), enemy reward logic, wave progression, and endgame win/loss logic.",
           images: [
-            "/public/level_editor.PNG",
-              "/public/place_tower.PNG",
-              "/public/gameplay.PNG",
+            "/level_editor.PNG",
+              "/place_tower.PNG",
+              "/gameplay.PNG",
 
           ]
         },
@@ -59,11 +59,11 @@ export default {
           title: "Found Hound",
           description: "A full-stack web application that helps users find their ideal dog match by considering their preferences and swiping behavior. Built with Java, Spring Boot, Vue.js, SQL, and a RESTful API, Found Hound streamlines the adoption process by intelligently recommending dogs based on user interactions. The intuitive interface, powered by HTML, CSS, and JavaScript, provides a seamless experience for discovering, swiping, and connecting with potential furry companions.",
           images: [
-            "/public/found.PNG",
-            "/public/found_found_home.PNG",
-            "/public/found_hound_swipe.PNG",
-              "/public/landingpage.PNG",
-              "/public/login.PNG",
+            "/found.PNG",
+            "/found_found_home.PNG",
+            "/found_hound_swipe.PNG",
+              "/landingpage.PNG",
+              "/login.PNG",
 
 
           ],
